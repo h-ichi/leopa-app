82e2843eee6447d5029d11afa66ee37ed435e0f0
h-ichi <h.ichimiya.dosapo@gmail.com>
2026-03-16 13:26:38 +0900

    ルーター変更

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,21 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Terms from "./pages/Terms";
 import Privacy from "./pages/Privacy";
 import LeopaCalendar from "./components/LeopaCalendar";
+
+
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        {/* LP（トップページ） */}
         <Route path="/" element={<LandingPage />} />
-
-        {/* カレンダー本体 */}
         <Route path="/app" element={<LeopaCalendar />} />
         <Route path="/terms" element={<Terms />} />
-<Route path="/privacy" element={<Privacy />} />
+        <Route path="/privacy" element={<Privacy />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
